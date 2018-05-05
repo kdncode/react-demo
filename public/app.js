c0379436c4f750588c4db5a90ccdfcda8dcbf25b
kdncode <kdncode@gmail.com>
2018-05-04 21:54:23 -0500

    added special props - children

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ class App extends React.Component {
         return(
             <div>
                 <h1>Hello zzz {this.props.name} - {this.props.grade} </h1>
+                <p>{this.props.children}</p>
                 <Header1 />
             </div>
         );
@@ -21,7 +22,7 @@ class Header1 extends React.Component{
 
 ReactDOM.render( 
     <div>
-        <App name="ReactJS Only" grade="2nd"/>
-        <App name="AngularJS Only" grade="5th"/>
+        <App name="ReactJS Only" grade="2nd">$19.99</App>
+        <App name="AngularJS Only" grade="5th">$39.99</App>
     </div>,
     document.getElementById("root") );
