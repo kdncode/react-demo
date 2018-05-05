cb4f6a1f915f805e14aec8b9d6d518f88f644919
kdncode <kdncode@gmail.com>
2018-05-04 21:46:54 -0500

    added Nested component

--- public/app.js
+++ public/app.js
@@ -3,11 +3,25 @@ class App extends React.Component {
         return(
             <div>
                 <h1>Hello zzz</h1>
+                <Header1 />
             </div>
         );
     }
 };
 
+class Header1 extends React.Component{
+    render() {
+        return(
+            <div>
+                <p>This is a nested react</p>
+            </div>
+        )
+    }
+}
+
 ReactDOM.render( 
-    <App />, 
+    <div>
+        <App />
+        <App />
+    </div>,
     document.getElementById("root") );
