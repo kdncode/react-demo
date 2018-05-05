984870f1c52cfaeabc839835b99a25fd7e2a3eae
kdncode <kdncode@gmail.com>
2018-05-04 22:10:03 -0500

    Event handler

--- public/app.js
+++ public/app.js
@@ -1,9 +1,19 @@
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.getInfo = this.getInfo.bind(this);
+    }
+
+    getInfo(){
+        alert('helo');
+    }
+
     render() {
         return(
             <div>
                 <h1>Hello zzz {this.props.name} - {this.props.grade} </h1>
                 <p>{this.props.children}</p>
+                <button onClick={this.getInfo}>More Info</button>
                 <Header1 />
             </div>
         );
